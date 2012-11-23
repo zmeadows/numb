2d4d0fba3976869e1078ca507199a6886f6d7013
Zac Meadows <zmeadows@gmail.com>
2012-11-23 01:50:39 -0600

    including stdlib.h

--- src/base.h
+++ src/base.h
@@ -1,6 +1,8 @@
 #ifndef BASE_H
 #define BASE_H
 
+#include "stdlib.h"
+
 typedef double real_t;
 
 /* real_f represents a function from reals to reals */
@@ -11,5 +13,9 @@ typedef double (*reals_f)(double*);
 
 /* system of ODE's */
 typedef reals_f *ode_system;
+
+double rand_double(double min, double max);
+
+real_t signum(real_t n);
 
 #endif
