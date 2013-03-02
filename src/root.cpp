a849941eeb6d993fedb98fc64564b6f484550077
Zac Meadows <zak@zak-VirtualBox.(none)>
2013-03-02 12:10:31 -0600

    cleaning up root.cpp/h

--- src/root.cpp
+++ src/root.cpp
@@ -1,43 +1,51 @@
 #include "root.h"
-/*
-// FINDER BASE CLASS
-Finder::Finder(realfunc eqn, double tol, int max_iter)
+
+RootFinder::RootFinder(func_R_R eqn, double tol, int max_iter)
     : current_iteration(0),
       max_iterations(max_iter),
       tolerance(tol),
       equation(eqn),
-      root(0),
-      debug(false),
-      log_output(false) {}
+      root(0)
+{
+};
 
-std::pair <bool,double> Finder::find_root(void) {
+std::pair<bool,double>
+RootFinder::find_root(void)
+{
     std::pair <bool,double> result;
     while (current_iteration < max_iterations) {
         if (iterate()) return (std::make_pair(true,root));
         ++current_iteration;
     }
     return (std::make_pair(false,root));
-}
+};
 
-// BISECTION METHOD
-Bisection::Bisection(realfunc eqn, double x_min, double x_max, double tol, int max_iter)
-    : Finder(eqn, tol, max_iter),
+Bisection::Bisection(func_R_R eqn, double x_min, double x_max, double tol, int max_iter)
+    : RootFinder(eqn, tol, max_iter),
       a(x_min),
-      b(x_max) {}
+      b(x_max)
+{
+};
 
-bool Bisection::iterate(void) {
+bool
+Bisection::iterate(void)
+{
     root = (a + b)/2;
     if (equation(root) == 0 || (b - a)/2 < tolerance) return true;
     (equation(a)*equation(root)<0) ? b = root : a = root;
     return false;
-}
+};
 
-// NEWTON'S METHOD
-Newton::Newton(realfunc _equation, realfunc _derivative, double guess, double _tolerance, int _max_iterations)
-    : Finder(_equation, _tolerance, _max_iterations),
+Newton::Newton(func_R_R _equation, func_R_R _derivative, double guess, double _tolerance, int _max_iterations)
+    : RootFinder(_equation, _tolerance, _max_iterations),
       derivative(_derivative),
-      prev(guess) {}
+      prev(guess)
+{
+};
 
-bool Newton::iterate(void) {
+bool
+Newton::iterate(void)
+{
     return false;
-}*/
+};
+
--- src/root.h
+++ src/root.h
@@ -4,41 +4,39 @@
 #include "base.h"
 #include "stdio.h"
 #include <utility>
-/*
-// base root-finding class that each type of root-finder inherits from
-class Finder {
+
+/* base root-finding class */
+class RootFinder {
   protected:
     int current_iteration;
     int max_iterations;
     double tolerance;
-    realfunc equation;
+    func_R_R equation;
     double root;
-    bool debug;
-    bool log_output;
     virtual bool iterate(void) = 0;
   public:
-    std::pair <bool,double> find_root(void);
-    Finder(realfunc eqn, double tol, int max_iter);
+    std::pair<bool,double> find_root(void);
+    RootFinder(func_R_R eqn, double tol, int max_iter);
 };
 
 // classic bisection method
-class Bisection : public Finder {
+class Bisection : public RootFinder {
   private:
     double a;
     double b;
     bool iterate(void);
   public:
-    Bisection(realfunc eqn, double x_min, double x_max,  double tol, int max_iter);
+    Bisection(func_R_R eqn, double x_min, double x_max,  double tol, int max_iter);
 };
 
 // newton's method
-class Newton : public Finder {
+class Newton : public RootFinder {
   private:
     bool iterate(void);
-    realfunc derivative;
+    func_R_R derivative;
     double prev;
   public:
-    Newton(realfunc _equation, realfunc _derivative, double guess, double _tolerance, int _max_iterations);
+    Newton(func_R_R _equation, func_R_R _derivative, double guess, double _tolerance, int _max_iterations);
 };
-*/
+
 #endif
